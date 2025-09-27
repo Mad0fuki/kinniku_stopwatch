e0861e3ea1727e840f3d5b96154c7b2443c0099c
Mad0fuki <madowipe@gmail.com>
2025-09-28 08:33:39 +0900

    Moved service-worker.js to src

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-//serviceWorkerRegistration.register();
+serviceWorkerRegistration.register();
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
